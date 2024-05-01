88485c36c6375ea569c8f2c7b9f92dc8e1bea4a8
Xinxin Wei <xinxinw@imaginet.com>
2024-05-01 14:13:05 -0500

    temp save

---
+++ meals_app/lib/providers/favorites_provider.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meals_app/models/meal.dart';
+
+class FavoriteMealsNotifier extends StateNotifier<List<Meal>> {
+  FavoriteMealsNotifier() : super([]);
+
+  bool toggleMealFavoriteStatus(Meal meal) {
+    final mealIsFavorite = state.contains(meal);
+
+    if (mealIsFavorite) {
+      state = state.where((m) => m.id != meal.id).toList();
+      return false;
+    } else {
+      state = [...state, meal];
+      return true;
+    }
+  }
+
+  bool isFavoriteMeal(Meal meal) {
+    return state.contains(meal);
+  }
+}
+
+final favoritedMealsProvider =
+    StateNotifierProvider<FavoriteMealsNotifier, List<Meal>>((ref) {
+  return FavoriteMealsNotifier();
+});
